6a62a0d5f43c05773ce7760ccedc3a3ae2f6fd15
Poojith <pmadduru@albany.edu>
2024-12-03 10:28:31 -0500

    chatbot issue

--- backend/routes/chatbot-router.js
+++ backend/routes/chatbot-router.js
@@ -3,51 +3,56 @@ import University from "../models/university-model.js";
 
 const chatbotRouter = express.Router();
 
-const questions = {
-  q1: {
-    id: "q1",
-    question: "Highest rated university",
-    answer: async () => {
-      const univ = await University.find({ overallRating: { $ne: null } })
-        .sort({ overallRating: "desc" })
-        .limit(1);
-
-      if (univ && univ[0]) {
-        return {
-          answer: `${univ[0].name}, is the highest rated.`,
-          univId: univ[0]._id,
-        };
-      }
-      return { answer: "Sorry, no enough information is available this time." };
-    },
-  },
-  q2: {
-    id: "q2",
-    question: "Least rated university",
-    answer: async () => {
-      const univ = await University.find({ overallRating: { $ne: null } })
-        .sort({ overallRating: "asc" })
-        .limit(1);
-      if (univ && univ[0]) {
-        return {
-          answer: `${univ[0].name}, is the least rated.`,
-          univId: univ[0]._id,
-        };
-      }
-      return { answer: "Sorry, no enough information is available this time." };
-    },
-  },
-};
-
 chatbotRouter.post("/:id?", async (req, res) => {
+  const questions = {
+    q1: {
+      id: "q1",
+      question: "Highest rated university",
+      answer: async () => {
+        const univ = await University.find({ overallRating: { $ne: null } })
+          .sort({ overallRating: "desc" })
+          .limit(1);
+
+        if (univ && univ[0]) {
+          return {
+            answer: `${univ[0].name}, is the highest rated.`,
+            univId: univ[0]._id,
+          };
+        }
+        return {
+          answer: "Sorry, no enough information is available this time.",
+        };
+      },
+    },
+    q2: {
+      id: "q2",
+      question: "Least rated university",
+      answer: async () => {
+        const univ = await University.find({ overallRating: { $ne: null } })
+          .sort({ overallRating: "asc" })
+          .limit(1);
+        if (univ && univ[0]) {
+          return {
+            answer: `${univ[0].name}, is the least rated.`,
+            univId: univ[0]._id,
+          };
+        }
+        return {
+          answer: "Sorry, no enough information is available this time.",
+        };
+      },
+    },
+  };
+
   const { question } = req.body;
   const faqId = req.params.id;
 
   if (faqId) {
-    const answer = questions[faqId];
+    let answer = questions[faqId];
     if (!answer) {
       return res.json({ answer: "Sorry, no information available this time." });
     }
+
     const ansResp = await answer.answer?.();
     return res.json(ansResp);
   }
