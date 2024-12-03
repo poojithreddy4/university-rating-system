3e70ff041e0e6836f4ab6389e119e1f69771d724
poojithreddy4 <pmadduru@albany.edu>
2024-12-02 20:56:52 -0500

    index html

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,9 @@ const STATIC_FILES_DIR = path.join(process.cwd(), "public");
 
 app.use(cors());
 app.use(express.json());
+app.use(
+  express.static(path.resolve(process.cwd(), "public"), { extensions: ["js"] })
+);
 
 // Database
 initDB();
