9ed63be84b1a0e5a1ea14799d941b2142b010fa3
poojithreddy4 <pmadduru@albany.edu>
2024-12-02 20:16:23 -0500

    backend folder changes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import cors from "cors";
 import express from "express";
+import path from "path";
 import initDB from "./db/index.js";
 import router from "./routes/index.js";
 
 const app = express();
+const STATIC_FILES_DIR = path.join(import.meta.dirname, "public");
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +15,10 @@ initDB();
 
 // Router
 router(app);
+
+app.get("*", (req, res) => {
+  return res.sendFile(path.join(STATIC_FILES_DIR, "index.html"));
+});
 
 // const server = app.listen(PORT, () => console.log("Listening on port", PORT));
 
